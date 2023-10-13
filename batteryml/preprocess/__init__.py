3ab7cfcd1dbf0e80d5074e2e238de81a0520573e
Han <zh950713@gmail.com>
2023-10-13 15:14:24 +0000

    add preprocess imports

--- batteryml/preprocess/__init__.py
+++ batteryml/preprocess/__init__.py
@@ -1,2 +1,14 @@
-from .download import Downloader
+from .download import Downloader, DOWNLOAD_LINKS
+from .preprocess_CALCE import CALCEPreprocessor
+from .preprocess_HNEI import HNEIPreprocessor
+from .preprocess_HUST import HUSTPreprocessor
 from .preprocess_MATR import MATRPreprocessor
+from .preprocess_OX import OXPreprocessor
+from .preprocess_RWTH import RWTHPreprocessor
+from .preprocess_SNL import SNLPreprocessor
+from .preprocess_UL_PUR import ULPURPreprocessor
+
+
+SUPPORTED_SOURCES = [
+    'CALCE', 'HNEI', 'HUST', 'MATR', 'OX', 'RWTH', 'SNL', 'UL_PUR'
+]
